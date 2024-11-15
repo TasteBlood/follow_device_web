25304d4e4513baf1a02bacc397cbc53823cb897e
TasteBlood <1303054271@qq.com>
2024-11-15 17:29:21 +0800

    导出最新的数据库文件

--- model/follow_device_db.sql
+++ model/follow_device_db.sql
@@ -11,11 +11,33 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 14/11/2024 17:50:37
+ Date: 15/11/2024 17:28:40
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_device
+-- ----------------------------
+DROP TABLE IF EXISTS `t_device`;
+CREATE TABLE `t_device`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `hospital_id` int(0) NOT NULL COMMENT '医院id',
+  `device_imei_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '设备imei',
+  `device_reg_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '设备注册码',
+  `is_update` int(0) NOT NULL DEFAULT 1 COMMENT '是否更新 1=是 -1=否',
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_device
+-- ----------------------------
+INSERT INTO `t_device` VALUES (15, 9, 'D1001', '39492', -1, '2024-11-15 09:01:50');
+INSERT INTO `t_device` VALUES (16, 9, 'D1002', '19985', -1, '2024-11-15 09:02:00');
+INSERT INTO `t_device` VALUES (17, 8, 'S0001', '88795', -1, '2024-11-15 09:02:19');
+INSERT INTO `t_device` VALUES (18, 8, 'S0002', '61607', -1, '2024-11-15 09:02:29');
 
 -- ----------------------------
 -- Table structure for t_hospital
@@ -32,13 +54,13 @@ CREATE TABLE `t_hospital`  (
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `create_time` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_hospital
 -- ----------------------------
-INSERT INTO `t_hospital` VALUES (1, '东山县卫生所', '徐希武', '18809446038', '甘肃省', '兰州市', '城关区', '电商孵化大厦', '2024-11-14 17:34:36');
-INSERT INTO `t_hospital` VALUES (2, '东山县卫生所', '徐希武', '18809446038', '甘肃省', '兰州市', '城关区', '电商孵化大厦', '2024-11-14 17:34:36');
+INSERT INTO `t_hospital` VALUES (8, '石门山卫生所', '张德贵', '18809446038', '甘肃省', '武威市', '古浪县', '黄羊川镇石门山村', '2024-11-15 09:00:13');
+INSERT INTO `t_hospital` VALUES (9, '马圈滩卫生所', '严私德', '15002509828', '甘肃省', '武威市', '古浪县', '黄羊川镇马圈滩村', '2024-11-15 09:01:32');
 
 -- ----------------------------
 -- Table structure for t_user
@@ -50,6 +72,6 @@ CREATE TABLE `t_user`  (
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
